ae104a94cfb1d5e3dde20ec202281a3b47269a62
Kalugin Dmitry <kalugin.d27o@gmail.com>
2021-12-02 11:18:35 +0300

    add main page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,103 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h1>
+            Дизайнер оборудования
+          </h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12 col-sm-4 d-flex justify-content-center">
+          <div class="card" style="width: 18rem;">
+            <div class="d-flex justify-content-center">
+              <img 
+                src="@/assets/icons/components.svg" 
+                class="card-img-top"
+                style="color: red; width: 75px; height 75px" 
+              >
+            </div>            
+            <div class="card-body">
+              <h5 class="card-title">Компоненты</h5>
+              <p class="card-text">
+                <span>
+                  Создание компонентов
+                </span> 
+              </p>
+              <a href="#" class="btn btn-primary">Перейти</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-sm-4 d-flex justify-content-center">
+          <div class="card" style="width: 18rem;">
+            <div class="d-flex justify-content-center">
+              <img 
+                src="@/assets/icons/warning.svg" 
+                class="card-img-top"
+                style="color: red; width: 75px; height 75px" 
+              >
+            </div>            
+            <div class="card-body">
+              <h5 class="card-title">Внимание!</h5>
+              <p class="card-text">
+                <span>
+                  Данный стенд является тестовым.
+                </span>
+                <span>
+                  Создан с целью демонстрации возможности создания оборудования и его компонентов.
+                </span> 
+              </p>
+              <span class="badge bg-danger">Не использовать в продакшене!</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-sm-4 d-flex justify-content-center">
+          <div class="card" style="width: 18rem;">
+            <div class="d-flex justify-content-center">
+              <img 
+                src="@/assets/icons/hardware.svg" 
+                class="card-img-top"
+                style="color: red; width: 75px; height 75px" 
+              >
+            </div>            
+            <div class="card-body">
+              <h5 class="card-title">Оборудование</h5>
+              <p class="card-text">
+                <span>
+                  Создание оборудования
+                </span> 
+              </p>
+              <a href="#" class="btn btn-primary">Перейти</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row mt-3">
+        <div class="col-12">
+          <h2>
+            При поддержке 
+          </h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">                
+          <div style="display: flex; justify-content: space-around;">
+            <a href="#">
+              <img src="@/assets/logo-mirea.png"  style="width: 100px; height: 100px">
+            </a>
+            <a href="#">
+              <img src="@/assets/logo-vuc.png"  style="width: 100px; height: 100px">
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
