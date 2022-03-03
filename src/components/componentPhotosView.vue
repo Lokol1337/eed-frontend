1dadeafb4cb956410b38130db036187aaeef4a81
Dmitry Kalugin <kalugin.d27o@gmail.com>
2022-03-03 11:33:05 +0300

    update componentPhotosView

--- src/components/componentPhotosView.vue
+++ src/components/componentPhotosView.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
-    <div v-for="(photo, j) in photos" :key="j">
+    <div v-for="(photo, i) in photos" :key="i">
       <img
         style="
           border-color: #86b7fe;
           box-shadow: 0 0 0 0.25rem rgb(13 110 253 / 25%);
+          max-width: 150px;
+          max-height: 150px;
         "
         class="img-fluid"
         :src="photo"
@@ -19,7 +21,7 @@ export default {
     photos: {
       type: Array,
       required: true,
-    }
-  }
-}
+    },
+  },
+};
 </script>
