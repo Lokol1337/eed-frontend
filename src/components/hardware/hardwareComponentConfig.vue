0426a2c4517f68127320be62308474abf6018c91
Dmitry Kalugin <kalugin.d27o@gmail.com>
2022-03-31 16:09:17 +0300

    separate config form to component

---
+++ src/components/hardware/hardwareComponentConfig.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <span>Компонент {{ selectedHardwareComponent.name }}</span>
+    <form class="border rounded p-2">
+      <div class="input-group mb-3">
+        <label>Ширина</label>
+        <input
+          v-model.number="selectedHardwareComponent.width"
+          type="number"
+          class="form-control"
+          placeholder="Ширина"
+          maxlength="255"
+        />
+      </div>
+      <div class="input-group mb-3">
+        <label>Высота</label>
+        <input
+          v-model.number="selectedHardwareComponent.height"
+          type="number"
+          class="form-control"
+          placeholder="Высота"
+          maxlength="255"
+        />
+      </div>
+      <div class="input-group mb-3">
+        <label>Top</label>
+        <input
+          v-model.number="selectedHardwareComponent.top"
+          type="number"
+          class="form-control"
+          placeholder="top"
+          maxlength="255"
+        />
+      </div>
+      <div class="input-group mb-3">
+        <label>Left</label>
+        <input
+          v-model.number="selectedHardwareComponent.left"
+          type="number"
+          class="form-control"
+          placeholder="left"
+          maxlength="255"
+        />
+      </div>
+      <div class="input-group mb-3">
+        <button class="btn btn-primary" @click.prevent="closeHardwareComponent">
+          Закрыть
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    closeHardwareComponent() {
+      this.$store.dispatch("ADD_SELECTED_COMPONENT", null);
+    },
+  },
+  computed: {
+    selectedHardwareComponent() {
+      return this.$store.getters.SELECTED_COMPONENT;
+    },
+  },
+};
+</script>
--- src/views/CreateHardware.vue
+++ src/views/CreateHardware.vue
@@ -10,76 +10,10 @@
           <hardwareCanvas />
         </div>
         <div v-if="!isHardwareComponentSelect" class="col-12 col-sm-2">
-          <!-- <div
-            v-for="(component, i) in components"
-            :key="i"
-            style="border: 1px solid"
-          >
-            <span>
-              {{ component.name }}
-            </span>
-            <div v-for="(photo, j) in component.photos" :key="j">
-              <img
-                :src="photo"
-                style="max-width: 190px; height: auto; cursor: pointer"
-                @click.prevent="addComponentToCanvas(component)"
-              />
-            </div>
-          </div> -->
           <componentList />
         </div>
         <div v-else class="col-12 col-sm-2">
-          <span>Компонент {{ selectedHardwareComponent.name }}</span>
-          <form class="border rounded p-2">
-            <div class="input-group mb-3">
-              <label>Ширина</label>
-              <input
-                v-model.number="selectedHardwareComponent.width"
-                type="number"
-                class="form-control"
-                placeholder="Ширина"
-                maxlength="255"
-              />
-            </div>
-            <div class="input-group mb-3">
-              <label>Высота</label>
-              <input
-                v-model.number="selectedHardwareComponent.height"
-                type="number"
-                class="form-control"
-                placeholder="Высота"
-                maxlength="255"
-              />
-            </div>
-            <div class="input-group mb-3">
-              <label>Top</label>
-              <input
-                v-model.number="selectedHardwareComponent.top"
-                type="number"
-                class="form-control"
-                placeholder="top"
-                maxlength="255"
-              />
-            </div>
-            <div class="input-group mb-3">
-              <label>Left</label>
-              <input
-                v-model.number="selectedHardwareComponent.left"
-                type="number"
-                class="form-control"
-                placeholder="left"
-                maxlength="255"
-              />
-            </div>
-            <div class="input-group mb-3">
-              <button
-                class="btn btn-primary"
-                @click.prevent="closeHardwareComponent"
-              >
-                Закрыть
-              </button>
-            </div>
-          </form>
+          <hardwareComponentConfig />
         </div>
       </div>
     </div>
@@ -90,6 +24,7 @@
 import hardwareForm from "@/components/hardware/hardwareForm.vue";
 import hardwareCanvas from "@/components/hardware/hardwareCanvas.vue";
 import componentList from "@/components/hardware/componentList.vue";
+import hardwareComponentConfig from "@/components/hardware/hardwareComponentConfig.vue";
 // TODO: ВСЕ положить в стор, осбенно при создания обородувания (чтобы проще обмениваться состоянмия)
 // TODO: отрицательные значения координат не должны вводиться при ручном вводе
 // TODO: навесить валидацию на все формы
@@ -103,37 +38,15 @@ import componentList from "@/components/hardware/componentList.vue";
 // TODO: порезать это все на маленькие компонеты, которые легко тестировать
 // TODO: пертащить тудусы в доску на гитхаб, а то какой-то стыд
 export default {
-  data() {
-    return {
-    };
-  },
   components: {
     hardwareForm,
     hardwareCanvas,
-    componentList
+    componentList,
+    hardwareComponentConfig,
   },
   methods: {
-    // addComponentToCanvas(componentFromLib) {
-    //   const img = new Image();
-    //   img.src = componentFromLib.photos[0];
-    //   const imgWidth = Number(img.width);
-    //   const imgHeight = Number(img.height);
-    //   const hardwareComponent = {
-    //     name: componentFromLib.name,
-    //     photo: componentFromLib.photos[0],
-    //     top: 0,
-    //     left: 0,
-    //     width: imgWidth,
-    //     height: imgHeight,
-    //   };
-    //   this.$store.dispatch("ADD_HARDWARE_COMPONENT", hardwareComponent);
-    //   this.$store.dispatch("ADD_SELECTED_COMPONENT", hardwareComponent);
-    // },
     selectComponentHandler(hardwareComponent) {
       this.$store.dispatch("ADD_SELECTED_COMPONENT", hardwareComponent);
-    },
-    closeHardwareComponent() {
-      this.$store.dispatch("ADD_SELECTED_COMPONENT", null);
     },
     createHardwareHandler(hardware) {
       hardware.hardwareComponents = this.hardwareComponents;
@@ -148,9 +61,6 @@ export default {
     selectedHardwareComponent() {
       return this.$store.getters.SELECTED_COMPONENT;
     },
-    // components() {
-    //   return this.$store.getters.COMPONENTS;
-    // },
     isHardwareComponentSelect() {
       if (this.selectedHardwareComponent) {
         return true;
