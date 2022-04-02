202c60a06ee15cebbdcc92d55210e2e1e7e990ec
Dmitry Kalugin <kalugin.d27o@gmail.com>
2022-04-02 15:12:48 +0300

    add switch component photos

--- src/components/hardware/componentList.vue
+++ src/components/hardware/componentList.vue
@@ -8,12 +8,18 @@
       <span>
         {{ component.name }}
       </span>
-      <div v-for="(photo, j) in component.photos" :key="j">
+      <div>
+        <button @click.prevent="prevImg">
+          prev
+        </button>
         <img
-          :src="photo"
+          :src="component.photos[imgIndex]"
           style="max-width: 190px; height: auto; cursor: pointer"
           @click.prevent="addComponentToCanvas(component)"
         />
+        <button @click.prevent="nextImg(component.photos.length)">
+          next
+        </button>
       </div>
     </div>
   </div>
@@ -21,13 +27,18 @@
 
 <script>
 export default {
+  data() {
+    return {
+      imgIndex: 0,
+    }
+  },
   methods: {
     addComponentToCanvas(componentFromLib) {
       const img = new Image();
       img.src = componentFromLib.photos[0];
       const imgWidth = Number(img.width);
       const imgHeight = Number(img.height);
-      
+
       const hardwareComponent = {
         name: componentFromLib.name,
         photo: componentFromLib.photos[0],
@@ -40,6 +51,16 @@ export default {
       this.$store.dispatch("ADD_HARDWARE_COMPONENT", hardwareComponent);
       this.$store.dispatch("ADD_SELECTED_COMPONENT", hardwareComponent);
     },
+    nextImg(photosArrLength) {
+      if (this.imgIndex < photosArrLength - 1) {
+        this.imgIndex++;
+      } 
+    },
+    prevImg() {
+      if (this.imgIndex > 0) {
+        this.imgIndex--;
+      }
+    }
   },
   computed: {
     components() {
