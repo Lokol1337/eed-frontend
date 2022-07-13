b1247863623b9e9da2c2ad4cce1a0c048e24f76e
Kalugin Dmitry <kalugin.d27o@gmail.com>
2022-07-13 10:08:18 +0300

    добавлена упаковка СС-О

--- src/views/P302O.vue
+++ src/views/P302O.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="p-302-0">
     <h1>P-302-0</h1>
+    <button @click.prevent="nextPack">
+      next pack
+    </button>
     <div class="container">
       <div class="row">
         <div class="col-12 col-sm-12">
           <div class="hardware-view-page__canvas-wrp">
             <hardwareCanvas
-              :hardwareComponents="P302O.blocks[0].components"
-              :bgImage="P302O.blocks[0].background"
-              :backgroundSettings="P302O.blocks[0].backgroundSettings"
+              :hardwareComponents="P302O.blocks[packIdx].components"
+              :bgImage="P302O.blocks[packIdx].background"
+              :backgroundSettings="P302O.blocks[packIdx].backgroundSettings"
             />
           </div>
         </div>
@@ -26,9 +29,23 @@ export default {
   components: {
     hardwareCanvas,
   },
+  data() {
+    return {
+      packIdx: 0,
+    };
+  },
   computed: {
     P302O() {
       return P302OJSON;
+    }
+  },
+  methods: {
+    nextPack() {
+      if ((this.packIdx + 1) === this.P302O.blocks.length) {
+        this.packIdx = 0;
+      } else {
+        this.packIdx++;
+      }
     }
   }
 }
