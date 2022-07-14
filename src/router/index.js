e17a7112aec1b0c58aee7c70b4d675f528ce28c0
Kalugin Dmitry <kalugin.d27o@gmail.com>
2022-07-14 12:25:20 +0300

    Настроен деплой на GH Pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
-import Home from '@/views/Home.vue';
-import CreateComponent from '@/views/CreateComponent.vue';
-import CreateHardware from '@/views/CreateHardware.vue';
-import HardwaresView from '@/views/HardwaresView.vue';
-import Login from '@/views/Login.vue';
+// import Home from '@/views/Home.vue';
+// import CreateComponent from '@/views/CreateComponent.vue';
+// import CreateHardware from '@/views/CreateHardware.vue';
+// import HardwaresView from '@/views/HardwaresView.vue';
+// import Login from '@/views/Login.vue';
 
 // not designer hardware, demo page P-302-O
 import P3020 from '@/views/P302O.vue';
@@ -13,31 +13,31 @@ import P3020 from '@/views/P302O.vue';
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/create-component',
-    name: 'createComponent',
-    component: CreateComponent,
-  },
-  {
-    path: '/create-hardware',
-    name: 'createHardware',
-    component: CreateHardware,
-  },
-  {
-    path: '/hardwares-view',
-    name: 'hardwaresView',
-    component: HardwaresView,
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: Login,
-  },
+  // {
+  //   path: '/',
+  //   name: 'Home',
+  //   component: Home,
+  // },
+  // {
+  //   path: '/create-component',
+  //   name: 'createComponent',
+  //   component: CreateComponent,
+  // },
+  // {
+  //   path: '/create-hardware',
+  //   name: 'createHardware',
+  //   component: CreateHardware,
+  // },
+  // {
+  //   path: '/hardwares-view',
+  //   name: 'hardwaresView',
+  //   component: HardwaresView,
+  // },
+  // {
+  //   path: '/login',
+  //   name: 'login',
+  //   component: Login,
+  // },
   {
     path: '/p-302-o',
     name: 'p-302-o',
@@ -46,8 +46,8 @@ const routes = [
 ];
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
+  // mode: 'history',
+  base: '/eed-frontend/',
   routes
 });
 
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-        ? 'eed-frontend'
-        : '/'
+    publicPath: process.env.NODE_ENV === "production" ? "/eed-frontend/" : "/",
 };
