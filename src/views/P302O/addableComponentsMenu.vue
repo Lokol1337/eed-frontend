d3d38343ae7f42de85f2ea5bfda768a2e0ca4348
Kalugin Dmitry <kalugin.d27o@gmail.com>
2022-07-18 09:51:25 +0300

    добавлен набросок меню перемычек

---
+++ src/views/P302O/addableComponentsMenu.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>menu</h1>
+    <div
+      v-for="(component, i) in addableComponents"
+      :key="i"
+      @click="selectComponent(component)"
+      class="menu"
+    >
+      <span>
+        {{ component.name }}
+      </span>
+      <img :src="component.valuesAndPhotos[0].photo" class="menu__img">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    addableComponents: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  methods: {
+    selectComponent(component) {
+      this.$emit("selectComponent", component);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.menu {
+  display: flex;
+  flex-direction: column;
+
+  &__img {
+    max-width: 50px;
+    max-height: 50px;
+  }
+}
+</style>
