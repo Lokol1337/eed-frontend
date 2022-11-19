b4cee7d7bb2c3ceeed94cd5d1391e989294d916d
lokol <drejfeld@gmail.com>
2022-11-19 10:48:59 +0300

    p3036

--- src/views/P3306.vue
+++ src/views/P3306.vue
@@ -1,11 +1,42 @@
 <template>
   <div class="p-302-0">
-    <h1>П-330-6</h1>
+    <div class="container-fluid pt-4">
+      <div class="row">
+        <div class="col-auto col-sm-auto col-md-auto col-lg-auto col-xl-auto p-0">
+            <div style="width: 85px;">
+              
+            </div>
+        </div>
+        <div class="col-auto">
+          <nav aria-label="breadcrumb hidden">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item">
+                <a href="/eed-frontend/#/main" class="svgHome">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 59 576 512" class="uk-icon-up2 uk-svg" width="20" height="20"><path d="M496 512H368a16 16 0 0 1-16-16V368a16 16 0 0 0-16-16h-96a16 16 0 0 0-16 16v128a16 16 0 0 1-16 16H80a16 16 0 0 1-16-16V311c1.78-1.21 3.85-1.89 5.47-3.35L288 115l218.74 192.9c1.54 1.38 3.56 2 5.26 3.2V496a16 16 0 0 1-16 16z" class="fa-secondary"></path><path d="M527.92 283.88L298.6 81.61a16 16 0 0 0-21.17 0L48.11 283.89a16 16 0 0 1-22.59-1.21L4.1 258.89a16 16 0 0 1 1.21-22.59l256-226a39.85 39.85 0 0 1 53.45 0L416 99.67V48a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v136.43l58.69 51.83a16 16 0 0 1 1.22 22.59l-21.4 23.82a16 16 0 0 1-22.59 1.21z" class="fa-primary"></path></svg>
+                </a>
+              </li>
+              <li class="breadcrumb-item">
+                П-330-6
+              </li>
+              <li class="breadcrumb-item">
+                {{actualPack.name}}
+              </li>
+            </ol>
+          </nav>
+        </div>
+      </div>
+    </div>
+    <div>
+      <VueSidebarMenuAkahon @selectPackParent="selectPackHandler"/>
+    </div>
     <packManager :packs="allPacks.blocks" @selectPack="selectPackHandler"/>
-    <h3>{{ actualPack.name }}</h3>
     <div class="container-fluid">
       <div class="row">        
-        <div class="col-12 col-sm-12 p-0">
+        <div class="col-auto col-sm-auto col-md-auto col-lg-auto col-xl-auto p-0">
+          <div style="width: 85px;">
+          </div>
+        </div>
+        <div class="col-9 col-sm-9 col-md-10 col-lg-11 col-xl-11 p-0" >
           <div id="mainBlock" class="hardware-view-page__canvas-wrp" style="zoom:100%">
             <hardwareCanvas
               v-for="pack in allPacks.blocks"
@@ -25,7 +56,7 @@
 
 <script>
 import P302OJSON from "./P302O/P3306.json";
-
+import VueSidebarMenuAkahon from "./P302O/sideBerMenuP3036.vue";
 import hardwareCanvas from "./P302O/hardwareCanvas.vue";
 import packManager from "./P302O/packManager.vue";
 
@@ -35,6 +66,7 @@ export default {
     window.addEventListener('resize', this.updateWidth);
     this.allPacks = P302OJSON;
     this.actualPack = P302OJSON.blocks[0];
+    console.log();
   },
   mounted(){
     this.getFistZoom();
@@ -54,6 +86,7 @@ export default {
     }
   },
   components: {
+    VueSidebarMenuAkahon,
     hardwareCanvas,
     packManager,
     
@@ -74,10 +107,10 @@ export default {
       let firstZoom = 0;
       let imgWidth = 1000;
       if(this.width > imgWidth){
-        firstZoom = Math.floor((this.width - imgWidth)/imgWidth * 100) + 100 - 5;
+        firstZoom = Math.floor((this.width - imgWidth)/imgWidth * 100) + 100 - 10;
       }
       else if(this.width < imgWidth){
-        firstZoom = Math.ceil((this.width - imgWidth)/imgWidth * 100) + 100 - 5;
+        firstZoom = Math.ceil((this.width - imgWidth)/imgWidth * 100) + 100 - 10;
       }
       this.firstZoom = firstZoom;
       document.getElementById('mainBlock').style.zoom = this.firstZoom + '%';
