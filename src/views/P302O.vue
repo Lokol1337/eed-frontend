4c491c59cc4bca49f922d37dc7c7f1bd14d2432d
lokol <drejfeld@gmail.com>
2023-12-20 12:09:48 +0300

    new cabels & json

--- src/views/P302O.vue
+++ src/views/P302O.vue
@@ -200,11 +200,11 @@ export default {
       clearTimeout(this.timer)
     },
     goToPath(route, normative_id = 0, is_training = 1, min = 0, sec = 0) {
-      // window.location.reload();
       // console.log();
       // console.log(is_training + min + sec);
       console.log("NEW NORMATIVE ID", normative_id);
       this.$router.push({ path: route, query: { norm: normative_id, it: is_training, min: min, sec: sec } });
+      window.location.reload();
     },
     changeBlockYellow(apparat_id) {
       let packId = hwCmpHandler.findHardwareById(apparat_id, this.allPacks.blocks);
@@ -212,6 +212,7 @@ export default {
       this.rerenderStatmentSideBar++;
     },
     getNextExercisePathId() {
+      console.log("ФФФФФФФФФАВААА",this.$route.query.norm)
       if (parseInt(this.$route.query.norm) % 10 < 9)
         return parseInt(this.$route.query.norm) + 1;
       return -1;
@@ -267,9 +268,9 @@ export default {
     linkForNextExercise() {
       if (this.exerciseComplete) {
         this.stopTimer();
-        let nextExercisePathId = this.getNextExercisePathId();
-        if (nextExercisePathId != -1)
-          this.goToPath('/p-302-o', nextExercisePathId, this.is_tr, this.min, this.sec);
+        // let nextExercisePathId = this.getNextExercisePathId();
+        // if (nextExercisePathId != -1)
+        //   this.goToPath('/p-302-o', nextExercisePathId, this.is_tr, this.min, this.sec);
         return "";
       }
       return "d-none";
